662070007928dae60e85fa281696728abfac2f34
bilabila <bilabila@qq.com>
2018-01-13 21:00:20 +0800

    not found hint

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# my userscript
+# jav_r18
+
+add preview from r18.com to javlibrary.com
 
 support lastest chrome stable
+
+![](https://raw.githubusercontent.com/tkkcc/jav_r18/master/include/DeepinScreenshot_select-area_20180113201257.jpg)
+
--- jav_r18.js
+++ jav_r18.js
@@ -1,31 +1,35 @@
 // ==UserScript==
 // @name         jav_r18
-// @version      0.0.1
+// @version      0.0.2
 // @include      http*://*javlibrary.com/*/?v=*
 // @description  详情页添加r18.com预览
-// @homepageURL  https://github.com/tkkcc/userscript
-// @supportURL   https://github.com/tkkcc/userscript/issues
+// @supportURL   https://github.com/tkkcc/jav_r18/issues
 // @grant        GM_xmlhttpRequest
 // @grant        GM_download
 // ==/UserScript==
-(()=> {
+(function () {
     'use strict';
-    const parseHTML =  (str) => {
+    const parseHTML = str => {
         var tmp = document.implementation.createHTMLDocument();
         tmp.body.innerHTML = str;
         return tmp;
     };
     const avid = document.title.split(' ')[0];
+    const add_to_doc = video_url => {
+        const text = video_url ? `<video id='jav_r18' style='postiton:absolute;z-order:1' src=${video_url} controls autoplay></video>`
+            : '<div class="header" style="text-align:center;padding-top:1rem;">preview not found</div>';
+        document.querySelector('#video_favorite_edit').insertAdjacentHTML('afterend', text);
+    };
     const get_video_url = (avid, callback) => {
         GM_xmlhttpRequest({
             url: `http://www.r18.com/common/search/searchword=${avid}`,
-            onload: (res) => callback(parseHTML(res.responseText).querySelector('.js-view-sample').getAttribute('data-video-high'))
+            onload: res => {
+                try {
+                    callback(parseHTML(res.responseText).querySelector('.js-view-sample').getAttribute('data-video-high'));
+                } catch (err) { callback(''); }
+            }
         });
     };
-    const add_to_doc = (video_url) => {
-        document.querySelector('#video_favorite_edit').insertAdjacentHTML('beforeend',
-            `<video id='jav_r18' style='postiton:absolute;z-order:1' src=${video_url} controls autoplay></video>`);
-    };
-    if (document.querySelector('#video_jacket_img'))
+    if (document.querySelector('#video_jacket_img')) 
         get_video_url(avid, add_to_doc);
 })();
